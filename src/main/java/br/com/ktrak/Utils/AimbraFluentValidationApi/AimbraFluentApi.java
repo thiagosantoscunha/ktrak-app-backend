48c8332185e442f651a6186f2579370f82890f14
Thiago Cunha <thigosantoscunha@outlook.com>
2019-11-12 00:50:55 -0300

    Abstraindo temoporariamente

--- src/main/java/br/com/ktrak/Utils/AimbraFluentValidationApi/AimbraFluentApi.java
+++ src/main/java/br/com/ktrak/Utils/AimbraFluentValidationApi/AimbraFluentApi.java
@@ -5,5 +5,6 @@ import br.com.ktrak.Utils.AimbraFluentValidationApi.interfaces.INumberValidation
 import br.com.ktrak.Utils.AimbraFluentValidationApi.interfaces.ITextValidation;
 import br.com.ktrak.Utils.AimbraFluentValidationApi.interfaces.IValidationObject;
 
+
 public abstract class AimbraFluentApi implements ITextValidation, INumberValidation, IDataValidation, IValidationObject {
 }
