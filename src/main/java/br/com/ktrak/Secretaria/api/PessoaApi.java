0fc82d993bd1e4fa04a03a1393dc486237f6225d
Thiago Cunha <thigosantoscunha@outlook.com>
2019-11-24 16:10:15 -0300

    CRUD de Disciplina

---
+++ src/main/java/br/com/ktrak/Secretaria/api/PessoaApi.java
@@ -0,0 +1,26 @@
+package br.com.ktrak.secretaria.api;
+
+import br.com.ktrak.domain.dto.PessoaDto;
+import br.com.ktrak.domain.entities.PessoaEntity;
+import br.com.ktrak.domain.services.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@RequestMapping(path = "v1/pessoas")
+public class PessoaApi {
+
+    @Autowired
+    private PessoaService service;
+
+    @RequestMapping
+    public ResponseEntity<PessoaDto> buscaPorUsername(@PathParam("username") String username) {
+        var body = service.buscaPorUsername(username);
+        return ResponseEntity.ok(body);
+    }
+
+}
