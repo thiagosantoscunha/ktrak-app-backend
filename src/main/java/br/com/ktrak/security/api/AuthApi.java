9fb2a67f99a4fac36328d787da5463a6bcfa9f1f
Thiago Cunha <thigosantoscunha@outlook.com>
2019-11-29 01:10:59 -0300

    TRatamento da segurança da aplicação

--- src/main/java/br/com/ktrak/security/api/AuthApi.java
+++ src/main/java/br/com/ktrak/security/api/AuthApi.java
@@ -1,18 +1,12 @@
 package br.com.ktrak.security.api;
 
 
-import br.com.ktrak.domain.dto.AlunoDto;
-import br.com.ktrak.domain.dto.PessoaDto;
-import br.com.ktrak.security.dto.PayloadDto;
+import br.com.ktrak.domain.dto.AlunoLogadoDto;
+import br.com.ktrak.domain.dto.UserRequestDto;
 import br.com.ktrak.security.dto.UserDto;
 import br.com.ktrak.security.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,14 +17,8 @@ public class AuthApi {
     private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<AlunoDto> login(@RequestBody UserDto dto) {
-        return ResponseEntity.ok(authService.login(dto));
-    }
-
-    @GetMapping
-    public ResponseEntity<?> buscaUsuario(@AuthenticationPrincipal() UserDetails userDetails) {
-        System.out.println(userDetails);
-        return ResponseEntity.ok(userDetails);
+    public ResponseEntity<AlunoLogadoDto> login(@RequestBody UserRequestDto request) {
+        return ResponseEntity.ok(authService.login(request));
     }
 
 }
--- src/main/java/br/com/ktrak/security/services/AuthService.java
+++ src/main/java/br/com/ktrak/security/services/AuthService.java
@@ -4,6 +4,8 @@ import br.com.ktrak.Utils.Utils;
 import br.com.ktrak.domain.converters.AlunoConverter;
 import br.com.ktrak.domain.converters.UserConverter;
 import br.com.ktrak.domain.dto.AlunoDto;
+import br.com.ktrak.domain.dto.AlunoLogadoDto;
+import br.com.ktrak.domain.dto.UserRequestDto;
 import br.com.ktrak.domain.entities.AlunoEntity;
 import br.com.ktrak.domain.entities.PessoaEntity;
 import br.com.ktrak.domain.exceptions.BadRequestException;
@@ -15,12 +17,20 @@ import br.com.ktrak.security.repositories.AuthRepository;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Optional;
+
+import static br.com.ktrak.security.settings.SecurityConstants.EXPIRATION_TIME;
+import static br.com.ktrak.security.settings.SecurityConstants.SECRET;
 
 @Service
 public class AuthService {
@@ -37,13 +47,30 @@ public class AuthService {
     @Autowired
     private UserConverter converter;
 
-    public AlunoDto login(UserDto userDto) {
-        UserEntity user = authRepository.findByUsername(userDto.username);
-        if (!user.getPassword().equals(userDto.password)) {
+    public AlunoLogadoDto login(UserRequestDto userDto) {
+
+        UserEntity user = authRepository.findByUsername(userDto.getUsername());
+
+        if (user != null) {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            BCrypt.checkpw(userDto.getPassword(), user.getPassword());
+        } else {
             throw new BadRequestException("Usuário ou senha inválida");
         }
+
+        String token = Jwts.builder()
+                .setSubject(user.getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+
         PessoaEntity pessoa = pessoaRepository.findByUser(user);
-        return alunoConverter.toDto((AlunoEntity) pessoa);
+        pessoa.setUser(user);
+
+        AlunoLogadoDto alunoLogadoDto = new AlunoLogadoDto();
+        alunoLogadoDto.setAluno(alunoConverter.toDto((AlunoEntity) pessoa));
+        alunoLogadoDto.setToken(token);
+        return alunoLogadoDto;
     }
 
     public UserEntity saveByAluno(AlunoDto alunoDto) {
